9eeea4dc87cc04a8d4542f0de695e0384a893c63
brian <brian.ogango@student.moringaschool.com>
2022-12-10 17:21:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverseString(word);
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
+function reverseString(word) {
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
+
+
 
 /* 
   Add your pseudocode here
